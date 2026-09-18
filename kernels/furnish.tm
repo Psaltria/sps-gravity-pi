0f2c095c16747666c3fba0f7e550f05c2ae9358f
gavin <gavin.blumen@gmail.com>
2026-09-18 11:06:19 -0500

    cspice

---
+++ kernels/furnish.tm
@@ -0,0 +1,22 @@
+KPL/MK
+
+         File name: furnsh.tm
+
+         Here are the SPICE kernels required for my application
+         program.
+
+         Note that kernels are loaded in the order listed. Thus
+         we need to list the highest priority kernel last.
+
+
+         \begindata
+
+         KERNELS_TO_LOAD = 
+				  (
+            '/kernels/naif0008.tls'
+            '/kernels/de442s.dsp' 
+				  )
+
+         \begintext
+
+         End of meta-kernel
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
 #include "cam/Camera_system.h"
 
+#include <SpiceUsr.h>
+
+#include <chrono>
 #include <print>
+#include <string>
 #include <vector>
 
 int main() {
 	std::println("Hello World!");
 
-	const auto camera { Camera_system::acquire_camera() };
+	//const auto camera { Camera_system::acquire_camera() };
+	//auto image { camera.capture_photo() };
 
-	auto image { camera.capture_photo() };
+	furnsh_c("kernels/naif0012.tls");
+	furnsh_c("kernels/de442s.bsp");
+
+	auto now { std::chrono::system_clock::now() };
+	const std::string now_iso {std::format("{:%FT%T}Z", now)};
+
+	double et {};
+	str2et_c(now_iso.c_str(), &et);
+
+	std::println("{}", now);
 }
